f86be6c614d1b8986e2998d77c1a8c4926d118e7
SupersushiMega <sascha.rutz@outlook.com>
2020-09-13 21:59:57 +0200

    almost fully functional Output for with easy output selection

--- AnalogPCGaugesPC/Form1.Designer.cs
+++ AnalogPCGaugesPC/Form1.Designer.cs
@@ -30,37 +30,32 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.CPU_Usage = new System.Diagnostics.PerformanceCounter();
             this.RAM_Usage = new System.Diagnostics.PerformanceCounter();
+            this.Gauge1Hard = new System.Windows.Forms.ComboBox();
+            this.Gauge1Sens = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.COMPORT = new System.Windows.Forms.Label();
+            this.SensorTypeLabel = new System.Windows.Forms.Label();
+            this.Gauge1SensType = new System.Windows.Forms.ComboBox();
+            this.Gauge2Hard = new System.Windows.Forms.ComboBox();
+            this.Gauge2SensType = new System.Windows.Forms.ComboBox();
+            this.Gauge2Sens = new System.Windows.Forms.ComboBox();
+            this.Gauge3SensType = new System.Windows.Forms.ComboBox();
+            this.Gauge3Sens = new System.Windows.Forms.ComboBox();
+            this.Gauge3Hard = new System.Windows.Forms.ComboBox();
+            this.Gauge4SensType = new System.Windows.Forms.ComboBox();
+            this.Gauge4Sens = new System.Windows.Forms.ComboBox();
+            this.Gauge4Hard = new System.Windows.Forms.ComboBox();
+            this.port = new System.Windows.Forms.ComboBox();
+            this.PortSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.CPU_Usage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.RAM_Usage)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(320, 121);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(368, 79);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // timer
             // 
@@ -88,14 +83,209 @@
             this.RAM_Usage.CategoryName = "Memory";
             this.RAM_Usage.CounterName = "% Committed Bytes In Use";
             // 
+            // Gauge1Hard
+            // 
+            this.Gauge1Hard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge1Hard.FormattingEnabled = true;
+            this.Gauge1Hard.Location = new System.Drawing.Point(78, 161);
+            this.Gauge1Hard.Name = "Gauge1Hard";
+            this.Gauge1Hard.Size = new System.Drawing.Size(121, 21);
+            this.Gauge1Hard.TabIndex = 2;
+            this.Gauge1Hard.SelectedIndexChanged += new System.EventHandler(this.Guage1Hard_SelectedIndexChanged);
+            // 
+            // Gauge1Sens
+            // 
+            this.Gauge1Sens.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge1Sens.FormattingEnabled = true;
+            this.Gauge1Sens.Location = new System.Drawing.Point(78, 224);
+            this.Gauge1Sens.Name = "Gauge1Sens";
+            this.Gauge1Sens.Size = new System.Drawing.Size(121, 21);
+            this.Gauge1Sens.TabIndex = 3;
+            this.Gauge1Sens.SelectedIndexChanged += new System.EventHandler(this.Guage1Sens_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Component";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sensor";
+            // 
+            // COMPORT
+            // 
+            this.COMPORT.AutoSize = true;
+            this.COMPORT.Location = new System.Drawing.Point(6, 15);
+            this.COMPORT.Name = "COMPORT";
+            this.COMPORT.Size = new System.Drawing.Size(37, 13);
+            this.COMPORT.TabIndex = 6;
+            this.COMPORT.Text = "PORT";
+            // 
+            // SensorTypeLabel
+            // 
+            this.SensorTypeLabel.AutoSize = true;
+            this.SensorTypeLabel.Location = new System.Drawing.Point(5, 201);
+            this.SensorTypeLabel.Name = "SensorTypeLabel";
+            this.SensorTypeLabel.Size = new System.Drawing.Size(67, 13);
+            this.SensorTypeLabel.TabIndex = 7;
+            this.SensorTypeLabel.Text = "Sensor Type";
+            this.SensorTypeLabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Gauge1SensType
+            // 
+            this.Gauge1SensType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge1SensType.FormattingEnabled = true;
+            this.Gauge1SensType.Location = new System.Drawing.Point(78, 193);
+            this.Gauge1SensType.Name = "Gauge1SensType";
+            this.Gauge1SensType.Size = new System.Drawing.Size(121, 21);
+            this.Gauge1SensType.TabIndex = 8;
+            this.Gauge1SensType.SelectedIndexChanged += new System.EventHandler(this.Guage1SensType_SelectedIndexChanged);
+            // 
+            // Gauge2Hard
+            // 
+            this.Gauge2Hard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge2Hard.FormattingEnabled = true;
+            this.Gauge2Hard.Location = new System.Drawing.Point(205, 161);
+            this.Gauge2Hard.Name = "Gauge2Hard";
+            this.Gauge2Hard.Size = new System.Drawing.Size(121, 21);
+            this.Gauge2Hard.TabIndex = 9;
+            this.Gauge2Hard.Tag = "";
+            this.Gauge2Hard.SelectedIndexChanged += new System.EventHandler(this.Guage1Hard_SelectedIndexChanged);
+            // 
+            // Gauge2SensType
+            // 
+            this.Gauge2SensType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge2SensType.FormattingEnabled = true;
+            this.Gauge2SensType.Location = new System.Drawing.Point(205, 193);
+            this.Gauge2SensType.Name = "Gauge2SensType";
+            this.Gauge2SensType.Size = new System.Drawing.Size(121, 21);
+            this.Gauge2SensType.TabIndex = 11;
+            this.Gauge2SensType.SelectedIndexChanged += new System.EventHandler(this.Guage1SensType_SelectedIndexChanged);
+            // 
+            // Gauge2Sens
+            // 
+            this.Gauge2Sens.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge2Sens.FormattingEnabled = true;
+            this.Gauge2Sens.Location = new System.Drawing.Point(205, 224);
+            this.Gauge2Sens.Name = "Gauge2Sens";
+            this.Gauge2Sens.Size = new System.Drawing.Size(121, 21);
+            this.Gauge2Sens.TabIndex = 10;
+            this.Gauge2Sens.SelectedIndexChanged += new System.EventHandler(this.Guage1Sens_SelectedIndexChanged);
+            // 
+            // Gauge3SensType
+            // 
+            this.Gauge3SensType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge3SensType.FormattingEnabled = true;
+            this.Gauge3SensType.Location = new System.Drawing.Point(332, 193);
+            this.Gauge3SensType.Name = "Gauge3SensType";
+            this.Gauge3SensType.Size = new System.Drawing.Size(121, 21);
+            this.Gauge3SensType.TabIndex = 14;
+            this.Gauge3SensType.SelectedIndexChanged += new System.EventHandler(this.Guage1SensType_SelectedIndexChanged);
+            // 
+            // Gauge3Sens
+            // 
+            this.Gauge3Sens.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge3Sens.FormattingEnabled = true;
+            this.Gauge3Sens.Location = new System.Drawing.Point(332, 224);
+            this.Gauge3Sens.Name = "Gauge3Sens";
+            this.Gauge3Sens.Size = new System.Drawing.Size(121, 21);
+            this.Gauge3Sens.TabIndex = 13;
+            this.Gauge3Sens.SelectedIndexChanged += new System.EventHandler(this.Guage1Sens_SelectedIndexChanged);
+            // 
+            // Gauge3Hard
+            // 
+            this.Gauge3Hard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge3Hard.FormattingEnabled = true;
+            this.Gauge3Hard.Location = new System.Drawing.Point(332, 161);
+            this.Gauge3Hard.Name = "Gauge3Hard";
+            this.Gauge3Hard.Size = new System.Drawing.Size(121, 21);
+            this.Gauge3Hard.TabIndex = 12;
+            this.Gauge3Hard.SelectedIndexChanged += new System.EventHandler(this.Guage1Hard_SelectedIndexChanged);
+            // 
+            // Gauge4SensType
+            // 
+            this.Gauge4SensType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge4SensType.FormattingEnabled = true;
+            this.Gauge4SensType.Location = new System.Drawing.Point(459, 193);
+            this.Gauge4SensType.Name = "Gauge4SensType";
+            this.Gauge4SensType.Size = new System.Drawing.Size(121, 21);
+            this.Gauge4SensType.TabIndex = 17;
+            this.Gauge4SensType.SelectedIndexChanged += new System.EventHandler(this.Guage1SensType_SelectedIndexChanged);
+            // 
+            // Gauge4Sens
+            // 
+            this.Gauge4Sens.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge4Sens.FormattingEnabled = true;
+            this.Gauge4Sens.Location = new System.Drawing.Point(459, 224);
+            this.Gauge4Sens.Name = "Gauge4Sens";
+            this.Gauge4Sens.Size = new System.Drawing.Size(121, 21);
+            this.Gauge4Sens.TabIndex = 16;
+            this.Gauge4Sens.SelectedIndexChanged += new System.EventHandler(this.Guage1Sens_SelectedIndexChanged);
+            // 
+            // Gauge4Hard
+            // 
+            this.Gauge4Hard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Gauge4Hard.FormattingEnabled = true;
+            this.Gauge4Hard.Location = new System.Drawing.Point(459, 161);
+            this.Gauge4Hard.Name = "Gauge4Hard";
+            this.Gauge4Hard.Size = new System.Drawing.Size(121, 21);
+            this.Gauge4Hard.TabIndex = 15;
+            this.Gauge4Hard.SelectedIndexChanged += new System.EventHandler(this.Guage1Hard_SelectedIndexChanged);
+            // 
+            // port
+            // 
+            this.port.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.port.FormattingEnabled = true;
+            this.port.Location = new System.Drawing.Point(47, 12);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(121, 21);
+            this.port.TabIndex = 18;
+            this.port.SelectedIndexChanged += new System.EventHandler(this.port_SelectedIndexChanged);
+            // 
+            // PortSearch
+            // 
+            this.PortSearch.Location = new System.Drawing.Point(174, 10);
+            this.PortSearch.Name = "PortSearch";
+            this.PortSearch.Size = new System.Drawing.Size(75, 23);
+            this.PortSearch.TabIndex = 19;
+            this.PortSearch.Text = "Search";
+            this.PortSearch.UseVisualStyleBackColor = true;
+            this.PortSearch.Click += new System.EventHandler(this.PortSearch_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(677, 274);
+            this.Controls.Add(this.PortSearch);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.Gauge4SensType);
+            this.Controls.Add(this.Gauge4Sens);
+            this.Controls.Add(this.Gauge4Hard);
+            this.Controls.Add(this.Gauge3SensType);
+            this.Controls.Add(this.Gauge3Sens);
+            this.Controls.Add(this.Gauge3Hard);
+            this.Controls.Add(this.Gauge2SensType);
+            this.Controls.Add(this.Gauge2Sens);
+            this.Controls.Add(this.Gauge2Hard);
+            this.Controls.Add(this.Gauge1SensType);
+            this.Controls.Add(this.SensorTypeLabel);
+            this.Controls.Add(this.COMPORT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Gauge1Sens);
+            this.Controls.Add(this.Gauge1Hard);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -108,14 +298,29 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Diagnostics.PerformanceCounter CPU_Usage;
         private System.Diagnostics.PerformanceCounter RAM_Usage;
+        private System.Windows.Forms.ComboBox Gauge1Hard;
+        private System.Windows.Forms.ComboBox Gauge1Sens;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label COMPORT;
+        private System.Windows.Forms.Label SensorTypeLabel;
+        private System.Windows.Forms.ComboBox Gauge1SensType;
+        private System.Windows.Forms.ComboBox Gauge2Hard;
+        private System.Windows.Forms.ComboBox Gauge2SensType;
+        private System.Windows.Forms.ComboBox Gauge2Sens;
+        private System.Windows.Forms.ComboBox Gauge3SensType;
+        private System.Windows.Forms.ComboBox Gauge3Sens;
+        private System.Windows.Forms.ComboBox Gauge3Hard;
+        private System.Windows.Forms.ComboBox Gauge4SensType;
+        private System.Windows.Forms.ComboBox Gauge4Sens;
+        private System.Windows.Forms.ComboBox Gauge4Hard;
+        private System.Windows.Forms.ComboBox port;
+        private System.Windows.Forms.Button PortSearch;
     }
 }
 
--- AnalogPCGaugesPC/Form1.cs
+++ AnalogPCGaugesPC/Form1.cs
@@ -1,8 +1,11 @@
 ﻿using AnalogPCGaugesPC.Properties;
+using OpenHardwareMonitor.Hardware;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO.Ports;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Management;
@@ -14,6 +17,9 @@ namespace AnalogPCGaugesPC
 {
     public partial class Form1 : Form
     {
+        Computer comp;
+        ISensor[] SelectSensor;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,41 +27,89 @@ namespace AnalogPCGaugesPC
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "hallo";
+            COMPORT.Text = "hallo";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            SelectSensor = new ISensor[4];
+
+            comp = new Computer()
+            {
+                HDDEnabled = true,
+                MainboardEnabled = true,
+                RAMEnabled = true,
+                GPUEnabled = true,
+                CPUEnabled = true
+            };
+            comp.Open();
+
             serialPort1.PortName = Properties.Settings.Default.COMPort;
             try
             {
                 serialPort1.Open();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                MessageBox.Show(ex.Message,"Fehler",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show(ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            foreach (var hardware in comp.Hardware)
+            {
+                Gauge1Hard.Items.Add(hardware.Name);
+                Gauge2Hard.Items.Add(hardware.Name);
+                Gauge3Hard.Items.Add(hardware.Name);
+                Gauge4Hard.Items.Add(hardware.Name);
             }
             timer.Start();
-
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            GetComponent("Win32_Processor", "Name");
-            GetComponent("Win32_VideoController", "Name");
-            GetComponent("Win32_TemperatureProbe", "CurrentReading");
-            Console.Read();
             byte[] SerialOut;
             SerialOut = new byte[4];
             byte i;
-            for (i = 0; i < SerialOut.Length; i++)
+
+            foreach (var hardware in comp.Hardware)
             {
-                SerialOut[i] = 0;
+                hardware.Update();
             }
-            double CPU_Use = (CPU_Usage.NextValue() * 2.55);
-            double RAM_Use = (RAM_Usage.NextValue() * 2.55);
-            SerialOut[0] = (byte)CPU_Use;
-            SerialOut[1] = (byte)RAM_Use;
+
+            for (i = 0; i < SerialOut.Length - 1; i++)
+            {
+                if (SelectSensor[i] != null)
+                {
+                    if (SelectSensor[i].Value != null)
+                    {
+                        if (SelectSensor[i].SensorType == SensorType.Level || SelectSensor[i].SensorType == SensorType.Load || SelectSensor[i].SensorType == SensorType.Control || SelectSensor[i].SensorType == SensorType.Temperature)
+                        {
+                            SerialOut[i] = (byte)(SelectSensor[i].Value * (255 / 100));
+                        }
+
+                        else
+                        {
+                            SerialOut[i] = (byte)(SelectSensor[i].Value * (255 / SelectSensor[i].Max));
+                        }
+                        Debug.Write(SelectSensor[i].Name);
+                        Debug.Write(" ");
+                        Debug.Write(SelectSensor[i].SensorType);
+                        Debug.WriteLine(SelectSensor[i].Value);
+                        if (SerialOut[i] > 255)
+                        {
+                            SerialOut[i] = 255;
+                        }
+                    }
+                    else
+                    {
+                        SerialOut[i] = 0;
+                    }
+                }
+                else
+                {
+                    SerialOut[i] = 0;
+                }
+            }
+
             if (serialPort1.IsOpen)
             {
                 serialPort1.Write(SerialOut, 0, 4);
@@ -73,13 +127,200 @@ namespace AnalogPCGaugesPC
             this.Show();
         }
 
-        private static void GetComponent(string hwclass, string syntax)
+        private void Guage1Hard_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM " + hwclass);
+            string send = Convert.ToString(((ComboBox)sender).Name);
+            ComboBox Hard = Gauge1Hard;
+            ComboBox SensType = Gauge1SensType;
+            ComboBox Sens = Gauge1Sens;
+            bool foundCopy = false;
+            Debug.WriteLine(send);
 
-            foreach(ManagementObject mj in mos.Get())
+            if (send == "Gauge2Hard")
             {
-                Console.WriteLine(Convert.ToString(mj[syntax]));
+                Hard = Gauge2Hard;
+                SensType = Gauge2SensType;
+                Sens = Gauge2Sens;
+            }
+
+            else if (send == "Gauge3Hard")
+            {
+                Hard = Gauge3Hard;
+                SensType = Gauge3SensType;
+                Sens = Gauge3Sens;
+            }
+
+            else if (send == "Gauge4Hard")
+            {
+                Hard = Gauge4Hard;
+                SensType = Gauge4SensType;
+                Sens = Gauge4Sens;
+            }
+
+            Debug.WriteLine(Sens.Name);
+            Debug.WriteLine(SensType.Name);
+
+            Sens.Items.Clear();
+            SensType.Items.Clear();
+            foreach (var hardware in comp.Hardware)
+            {
+                if (hardware.Name == Convert.ToString(Hard.SelectedItem))
+                {
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        foundCopy = false;
+                        foreach (SensorType type in SensType.Items)
+                        {
+                            Debug.Write(type);
+                            if (type == sensor.SensorType)
+                            {
+                                foundCopy = true;
+                                break;
+                            }
+                        }
+                        if (!foundCopy)
+                        {
+                            SensType.Items.Add(sensor.SensorType);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Guage1SensType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string send = (((ComboBox)sender).Name);
+            byte Output = 0; 
+            ComboBox Hard = Gauge1Hard;
+            ComboBox SensType = Gauge1SensType;
+            ComboBox Sens = Gauge1Sens;
+
+            if (send == "Gauge2SensType")
+            {
+                Output = 1;
+                Hard = Gauge2Hard;
+                SensType = Gauge2SensType;
+                Sens = Gauge2Sens;
+            }
+
+            else if (send.ToString() == "Gauge3SensType")
+            {
+                Output = 2;
+                Hard = Gauge3Hard;
+                SensType = Gauge3SensType;
+                Sens = Gauge3Sens;
+            }
+
+            else if (send.ToString() == "Gauge4SensType")
+            {
+                Output = 3;
+                Hard = Gauge4Hard;
+                SensType = Gauge4SensType;
+                Sens = Gauge4Sens;
+            }
+
+            Sens.Items.Clear();
+            foreach (var hardware in comp.Hardware)
+            {
+                if (hardware.Name == Convert.ToString(Hard.SelectedItem))
+                {
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (Convert.ToString(sensor.SensorType) == Convert.ToString(SensType.SelectedItem))
+                        {
+                            Sens.Items.Add(sensor.Name);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Guage1Sens_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string send = (((ComboBox)sender).Name);
+            byte Output = 0;
+            ComboBox Hard = Gauge1Hard;
+            ComboBox SensType = Gauge1SensType;
+            ComboBox Sens = Gauge1Sens;
+
+            if (send == "Gauge2Sens")
+            {
+                Output = 1;
+                Hard = Gauge2Hard;
+                SensType = Gauge2SensType;
+                Sens = Gauge2Sens;
+            }
+
+            else if (send.ToString() == "Gauge3Sens")
+            {
+                Output = 2;
+                Hard = Gauge3Hard;
+                SensType = Gauge3SensType;
+                Sens = Gauge3Sens;
+            }
+
+            else if (send.ToString() == "Gauge4Sens")
+            {
+                Output = 3;
+                Hard = Gauge4Hard;
+                SensType = Gauge4SensType;
+                Sens = Gauge4Sens;
+            }
+
+            foreach(var hardware in comp.Hardware)
+            {
+                if (hardware.Name == Convert.ToString(Hard.SelectedItem))
+                {
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (Convert.ToString(sensor.SensorType) == Convert.ToString(SensType.SelectedItem))
+                        {
+                            if(sensor.Name == Convert.ToString(Sens.SelectedItem))
+                            {
+                                SelectSensor[Output] = sensor;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PortSearch_Click(object sender, EventArgs e)
+        {
+            string[] Ports = SerialPort.GetPortNames();
+            port.Items.AddRange(Ports);
+        }
+
+        private void port_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            Properties.Settings.Default.COMPort = port.SelectedItem.ToString();
+            serialPort1.PortName = port.SelectedItem.ToString();
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
